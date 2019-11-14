408f21593c13fda795b344706a1baa83a476d038
photonn <photonn@hotmail.com>
2019-11-14 16:13:01 +0100

    word count solution

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,21 +1,8 @@
-import string
-def count_words(sentence):
-    worddict = {}
-    splitedsentence = sentence.split( )
-    for word in splitedsentence:
-        cleanword = []
-        for char in word.lower():
-            if char.isalnum() or char == "-":
-                cleanword.append(char)
-        joined = ''.join(cleanword)
-        if len(joined) >= 1 and joined in worddict:
-            worddict[joined] = worddict[joined] + 1
-        else:
-            worddict[joined] = 1
-    print(worddict)
-    return worddict
+import re
+from collections import *
 
-count_words ("capullo esto esto5 esto^^ 456 es, es lo   que pasa's")
-    
-    #comprobar la funcion format de la claase string y las expresiones regex.
-    #tiene que haber una forma mas sencilla de atacar esto...
+def count_words(sentence):
+    sentence = re.sub("_", " ", sentence.lower())
+    inspected = re.findall(r"([\w]+[']?[\w]+|\d|[a-z])", sentence.lower())   
+    return Counter(inspected)
+pass
